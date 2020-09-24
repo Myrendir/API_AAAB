b648d4ef46431840635579e867e0d64626eb8aae
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-09-24 17:07:39 +0200

    Add the entity Users and install package Gedmo

---
+++ src/Entity/Users.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: shadowluffy
+ * Date: 9/24/20
+ * Time: 4:50 PM
+ */
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=UsersRepository::class)
+ *
+ * @author CONTE Alexandre <pro.alexandre.conte@gmail.com>
+ */
+class Users
+{
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $summonerName;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $availability;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isEnabled;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSummonerName(): ?string
+    {
+        return $this->summonerName;
+    }
+
+    public function setSummonerName(string $summonerName): self
+    {
+        $this->summonerName = $summonerName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getAvailability(): ?bool
+    {
+        return $this->availability;
+    }
+
+    public function setAvailability(bool $availability): self
+    {
+        $this->availability = $availability;
+
+        return $this;
+    }
+
+    public function getIsEnabled(): ?bool
+    {
+        return $this->isEnabled;
+    }
+
+    public function setIsEnabled(bool $isEnabled): self
+    {
+        $this->isEnabled = $isEnabled;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+}
