3fcac8a1e5424bb8edac6628e28a616035a7a402
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-10-13 19:57:05 +0200

    Initialization doc and bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
