34e025f4ce954398bd21d96afb82594b1a2b3fc7
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-10-23 18:52:58 +0200

    Add the functional test

---
+++ tests/functional/FakeTest.php
@@ -0,0 +1,24 @@
+<?php namespace App\Tests;
+
+class FakeTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \App\Tests\FunctionalTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $test = 'hello';
+        $this->assertEquals('hello', $test);
+    }
+}
