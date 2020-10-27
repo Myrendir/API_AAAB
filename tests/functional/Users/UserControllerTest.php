1eee9e59d6bab457823234549c875d38f53b18c4
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-10-27 15:40:41 +0100

    Update test functional

--- tests/functional/Users/UserControllerTest.php
+++ tests/functional/Users/UserControllerTest.php
@@ -31,6 +31,12 @@ class UserControllerTest extends \Codeception\Test\Unit
             'password' => 'michelle1',
             'confirmPassword' => 'michelle1'
         ]);
+        $this->tester->sendPostJson('/user/register', [
+            'summonerName' => 'Alex',
+            'email' => 'alex@example.com',
+            'password' => 'michelle1',
+            'confirmPassword' => 'michelle1'
+        ]);
         /**
          * Try to recover the token with the new user
          */
@@ -39,6 +45,33 @@ class UserControllerTest extends \Codeception\Test\Unit
          * Request get for the url
          */
         $this->tester->sendGet('/api/user/list');
+        $this->tester->seeResponseContainsJson([
+            'email' => 'michel@example.com',
+            'email' => 'alex@example.com'
+        ]);
+        $this->tester->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function testGetUserBySummonerName()
+    {
+        /**
+         * Register a new user for Auth
+         */
+        $this->tester->sendPostJson('/user/register', [
+            'summonerName' => 'Michel',
+            'email' => 'michel@example.com',
+            'password' => 'michelle1',
+            'confirmPassword' => 'michelle1'
+        ]);
+        /**
+         * Try to recover the token with the new user
+         */
+        $this->tester->createAuthenticatedClient('Michel', 'michelle1');
+        /**
+         * Request get for the url
+         */
+        $this->tester->sendGet('/api/user/get/Michel');
+        $this->tester->seeResponseContainsJson(['summoner_name' => 'Michel']);
         $this->tester->seeResponseCodeIs(HttpCode::OK);
     }
 }
