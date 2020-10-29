91d150e2a3ea3279ebfd7a7df72aa2efcfe2860f
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-10-29 09:15:09 +0100

    Add the fixtures in entrypoint.sh

--- docker/php-fpm/entrypoint.dev.sh
+++ docker/php-fpm/entrypoint.dev.sh
@@ -3,4 +3,5 @@ composer install
 ./bin/console cache:clear
 ./bin/console assets:install
 ./bin/console doctrine:schema:update --force
+./bin/console doctrine:fixtures:load --no-interaction
 php-fpm -F
