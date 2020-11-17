21d5c95c11a0d4a3d7c4045f0fcaa96cc80ee7ff
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-11-17 11:49:39 +0100

    Delete the env docker and admin login

--- docker/php-fpm/entrypoint.dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-composer install
-./bin/console cache:clear
-./bin/console assets:install
-./bin/console doctrine:schema:update --force
-./bin/console doctrine:fixtures:load --no-interaction
-php-fpm -F
